099b65fc7ba873e8ce3ccf094b5197d0ba01f5ca
RomainSa <romain.savidan@gmail.com>
2016-04-22 00:44:54 +0200

    Updated tensor shapes

--- convnet_statefarm.py
+++ convnet_statefarm.py
@@ -51,10 +51,10 @@ b_conv1 = bias_variable([n1])
 W_conv2 = weight_variable([4, 4, n1, n2])   # [patch_size1, patch_size2, input_channels, output_channels]
 b_conv2 = bias_variable([n2])
 
-W_conv3 = weight_variable([3, 3, n2, n3])   # [patch_size1, patch_size2, input_channels, output_channels]
+W_conv3 = weight_variable([3, 3, n2, n3])   # [patch_size1, patch_size2, p_channels, output_channels]
 b_conv3 = bias_variable([n3])
 
-W_fc1 = weight_variable([input_size[0] * input_size[1] * n3, n4])
+W_fc1 = weight_variable([15488, n4])
 b_fc1 = bias_variable([n4])
 
 W_fc2 = weight_variable([n4, 10])   # output size: 10
@@ -69,7 +69,7 @@ h_pool2_drop = tf.nn.dropout(h_conv2, keep_prob)
 h_conv3 = tf.nn.relu(conv2d(h_pool2_drop, W_conv3, s=1) + b_conv3)   # output size: None, 14, 14, n2
 h_pool3_drop = tf.nn.dropout(h_conv3, keep_prob)
 
-h_pool3_flat = tf.reshape(h_pool3_drop, [-1, input_size[0] * input_size[1] * n3])
+h_pool3_flat = tf.reshape(h_pool3_drop, [-1, 15488])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)   # output size: n3
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
